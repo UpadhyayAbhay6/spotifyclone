b27fa75f47fac80ff4ee35febab2eb92c4a74a01
UpadhyayAbhay7 <abhay9412541844@gmail.com>
2024-06-26 22:47:52 +0530

    changes

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ const playMusic = (track, pause = false) => {
 
 }
 async function displayAlbums(){
-    let a = await fetch(`http://127.0.0.1:5500/songsPlaylist/`);
+    let a = await fetch(`http://127.0.0.1:5500/spotifyclone/songsPlaylist/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
